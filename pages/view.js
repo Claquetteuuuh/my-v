0c9db64c529263179cb989dded9010f0aff7ff6f
Claquetteuuuh <75044328+Claquetteuuuh@users.noreply.github.com>
2022-03-22 19:56:49 -0700

    add navbar

--- pages/view.js
+++ pages/view.js
@@ -1,10 +1,20 @@
+import Head from 'next/head';
 import React from 'react';
+import Navbar from '../components/Navbar';
 import Player from '../components/Player';
+import styles from '../styles/View.module.css'
 
 const view = () => {
     return (
         <div>
-          <Player />  
+            <Head>
+                <title>MyV | Watch video</title>
+                <meta name="description" content="regardez des videos gratuitement" />
+                <link rel="icon" href="/img/logos/logo-black.png" />
+                <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'></link>
+            </Head>
+            <Navbar />
+            <Player />  
         </div>
     );
 };
