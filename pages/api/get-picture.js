debce77934504d7cc3164abcdc99fe4cb9bc75f2
Claquetteuuuh <75044328+Claquetteuuuh@users.noreply.github.com>
2022-04-11 12:34:11 -0700

    fix cookie decode bug

--- pages/api/get-picture.js
+++ pages/api/get-picture.js
@@ -1,16 +1,17 @@
 import dbConnect from "../../utils/dbConnect"
 import User from './schemas/User'
+import jwt_decode from 'jwt-decode'
 dbConnect()
 
 export default async function handler(req, res){
     if(req.method === 'GET'){
         if(req.headers.cookie){
-            res.status(200).json({message: 'cookie'})
-            // const id = JSON.parse(atob(req.headers.cookie.split('.')[1])).id
+            let token = req.headers.cookie.split('=')[1]
+            const {id } = jwt_decode(token)
 
-            // User.findOne({userId: id}).then(e => {
-            //     res.status(200).json({picture: e.picture, channel: e.username})
-            // }).catch((err) => res.status(400).json({error: err}))
+            User.findOne({userId: id}).then(e => {
+                res.status(200).json({picture: e.picture, channel: e.username})
+            }).catch((err) => res.status(400).json({error: err}))
 
         }else{
             res.status(403).json({error: 'not connected'})
