25c4355a954b9a800b40a044adbe1f388066b729
Claquetteuuuh <75044328+Claquetteuuuh@users.noreply.github.com>
2022-04-11 12:16:34 -0700

    Update get-picture.js

--- pages/api/get-picture.js
+++ pages/api/get-picture.js
@@ -10,7 +10,7 @@ export default async function handler(req, res){
 
             User.findOne({userId: id}).then(e => {
                 res.status(200).json({picture: e.picture, channel: e.username})
-            })
+            }).catch((err) => res.status(400).json({error: err}))
 
         }else{
             res.status(403).json({error: 'not connected'})
