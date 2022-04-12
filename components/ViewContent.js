6c37ed138db3f3fb96d9f25de680f86ba45d725a
Claquetteuuuh <75044328+Claquetteuuuh@users.noreply.github.com>
2022-04-11 18:16:30 -0700

    fix viewing page bug

---
+++ components/ViewContent.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react';
+import Player from '../components/Player';
+import styles from '../styles/ViewContent.module.css'
+import Video from '../components/Video'
+import axios from 'axios'
+
+const ViewContent = () => {
+        
+    const [data, setdata] = useState([]);
+
+    const dateParser = (date) => {
+
+        let newDate = new Date(date).toLocaleDateString('gp-GP', {
+            year: "numeric",
+            month: 'numeric',
+            day: 'numeric',
+        })
+        return newDate
+    }
+
+    useEffect(async () => {
+        await axios.get('/api/mongo-stream')
+        .then((res) => {
+            setdata(res.data)
+        })
+
+
+    }, [])
+
+    return (
+        <div className={styles.content}>
+                <div className={styles.videoInfo}>
+                    <Player />
+                </div>
+                <div className={styles.nextProposition}>
+                {
+                    data.map((video) => (
+                
+                        <div className={styles.uniqueVideo}>
+                            <Video 
+                                key={video.videoId}
+                                miniature={video.miniature}
+                                title={video.title}
+                                channelPicture={(video.channelPic)? video.channelPic : '/img/svg/random-user.jpg'}
+                                channelName={video.channel}
+                                views={video.views}
+                                date={(video.date)? dateParser(video.date): '00/00/00'}
+                                id={video.videoId}
+                            
+                            />
+                        </div>
+                    
+                    )).reverse()
+                }
+                </div>
+            
+            </div>
+    );
+};
+
+export default ViewContent;
