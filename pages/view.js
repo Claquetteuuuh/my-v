7376a15edcf63c89569b3ad49b2b372fd8ae5ac8
Claquetteuuuh <75044328+Claquetteuuuh@users.noreply.github.com>
2022-04-11 18:17:40 -0700

    Update view.js

--- pages/view.js
+++ pages/view.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import React from 'react';
 import ViewContent from '../components/ViewContent';
+import Navbar from '../components/Navbar'
 import styles from '../styles/view.module.css'
 
 const view = () => {
