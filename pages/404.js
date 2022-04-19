66ddb5e8413e0021414f61193b57ee60786a36a3
Kiwiii-dot <75044328+Kiwiii-dot@users.noreply.github.com>
2022-04-18 21:55:17 -0400

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Container404 from '../components/Container404';
 import Navbar from '../components/Navbar'
+import Head from 'next/head'
 
 const page404 = () => {
     return (
         <div>
+            <Head>
+            <title>MyV | Page no found</title>
+            <meta name="description" content="Cette page est un chemin de non retour" />
+            <link rel="icon" href="/img/logos/logo-black.png" />
+            </Head>
             <Navbar />
             <Container404 />
         </div>
