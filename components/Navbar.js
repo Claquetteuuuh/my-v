4ecb06bcc882c3d4e46fb4164a8b1bc41dd6a60f
Claquetteuuuh <75044328+Claquetteuuuh@users.noreply.github.com>
2022-04-28 13:50:20 -0700

    phone nav

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styles from '../styles/Navbar.module.css'
 import Link from 'next/link';
 import axios from 'axios'
@@ -7,9 +7,22 @@ const Navbar = (props) => {
     
     const [formValue, setFormValue] = useState('');
     const [userData, setuserData] = useState();
+    const [deployed, setDeployed] = useState(false)
+
+    const form = useRef(null)
+    const closeBtn = useRef(null)
 
     const deployResearch = () => {
+        form.style.display = 'flex'
+        form.style.width = '200px'
+        
+        setDeployed(true)
+    }
 
+    const closeResearch = () =>{
+        form.style.display = 'none'
+        form.style.width = '0'
+        setDeployed(false)
     }
 
     useEffect(() => {
@@ -30,12 +43,12 @@ const Navbar = (props) => {
             <div className={styles.left}>
                 <Link href="/"><img loading="eager" src='/img/logos/logo-gradient.png' height="47" width='55' alt={'Logo myV'} /></Link>
             </div>
-            <form onSubmit={e => formSub(e)} className={styles.input}>
+            <form ref={el => {form = el}} onSubmit={e => formSub(e)} className={styles.input}>
                 <input className={styles.text} value={formValue} onChange={e => setFormValue(e.target.value)} type="text" placeholder='rechercher une video...'/>
                 <input className={styles.submit} type='image' src={'/img/svg/search-outline.svg'} href={`/research?search=${formValue}`}/>
             </form>
             <div className={styles.right}>
-                <button onClick={() => {deployResearch()}} className={styles.buttonSearch}><img height={50} src={'/img/svg/search-outline.svg'} alt="research img" /></button>
+                {(!deployed)?<button onClick={() => {deployResearch()}} className={styles.buttonSearch}><img height={50} src={'/img/svg/search-outline.svg'} alt="research img" /></button>: <button ref={el => {closeBtn = el}} className={styles.closeButton} onClick={() => {closeResearch()}}>&times;</button>}
                 {(userData)? <Link href={`/channel?name=${userData.channel}`}><img loading="eager" height={50} width={50} src={(userData.picture)? userData.picture: '/img/svg/random-user.jpg' } className={styles.avatar} alt={`Picture of ${userData.channel}`} /></Link>: <Link href="/login"><img className={styles.loginButton} src="/img/svg/login.svg" height={40} width={40} /></Link>}
                 {(userData)? <Link href="/post-video"><img loading="eager" className={styles.addButton} src="/img/svg/add-button.svg" height={50} width={50} alt='add new video button' /></Link>: false}
             </div>
