b775db271564c2d42bcd1de3dda04cb3cab774fb
Claquetteuuuh <75044328+Claquetteuuuh@users.noreply.github.com>
2022-04-28 12:54:24 -0700

    Update EditVideoForm.js

--- components/EditVideoForm.js
+++ components/EditVideoForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from '../styles/editVideoForm.module.css'
+import styles from '../styles/EditVideoForm.module.css'
 
 const editVideoForm = () => {
     return (
