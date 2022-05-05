5c4ac663e099c3767205a66cc6a50358966508a4
Claquetteuuuh <75044328+Claquetteuuuh@users.noreply.github.com>
2022-05-05 05:43:17 -0700

    env

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-
-  env:{
-    cloudFlareAccount: "b34aad77a0649956f636aabd25654a21",
-    cloudFlareToken: "126696776eccea97c410b5b001deafebf6bdd",
-    cloudFlareEmail: "th.biabiany.dev@gmail.com",
-    cloudFlareAuthKey: "126696776eccea97c410b5b001deafebf6bdd",
-    mongodbPassword: "XmyABqJdssCfKqib",
-    mongo_uri: "mongodb+srv://claquetteuuuh:XmyABqJdssCfKqib@cluster0.fspow.mongodb.net/myVDatabase?retryWrites=true&w=majority"
-  },
-
-}
-
-module.exports = nextConfig
+    reactStrictMode: true,
+  
+    env:{
+      cloudFlareAccount: "b34aad77a0649956f636aabd25654a21",
+      cloudFlareToken: "126696776eccea97c410b5b001deafebf6bdd",
+      cloudFlareEmail: "th.biabiany.dev@gmail.com",
+      cloudFlareAuthKey: "126696776eccea97c410b5b001deafebf6bdd",
+      mongodbPassword: "XmyABqJdssCfKqib",
+      mongo_uri: "mongodb+srv://claquetteuuuh:XmyABqJdssCfKqib@cluster0.fspow.mongodb.net/myVDatabase?retryWrites=true&w=majority"
+    },
+  
+  }
+  
+  module.exports = nextConfig
